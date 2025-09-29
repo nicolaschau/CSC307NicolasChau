043d9fab65e1a7bf62c10bbc90b65775111edb2f
nicolaschau <nicolas5354@gmail.com>
2025-09-28 22:13:30 -0700

    Finished First React App

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -3,21 +3,5 @@ import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
-const [characters, setCharacters] = useState([]);
-
 function MyApp() {
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-  }
-
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form />
-    </div>
-  );
-}
-export default MyApp;
+  const [characters,
--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -7,8 +7,4 @@ import "./main.css";
 // Create the container
 const container = document.getElementById("root");
 
-// Create a root
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the Root
-root.render(<MyApp />);
+// Create a 
