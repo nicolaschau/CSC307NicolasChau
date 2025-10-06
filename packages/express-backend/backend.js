a4e4190ff3e4b7a3f91782ac30d5ec33e112e8c3
nicolaschau <nicolas5354@gmail.com>
2025-10-06 11:26:13 -0700

    Connected backend and frontend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -46,6 +47,7 @@ const addUser = (user) => {
   return user;
 };
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
